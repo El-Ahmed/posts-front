e642ffc5e5293bd4ac573cc1c1812870ce299893
Ahmed El herz <69149566+El-Ahmed@users.noreply.github.com>
2023-02-02 22:02:28 +0100

    added posts service

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,4 +1,7 @@
 import {Component, Input} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {PostService} from "../post.service";
+import {Post} from "../post";
 
 @Component({
   selector: 'app-create',
@@ -7,5 +10,17 @@ import {Component, Input} from '@angular/core';
 })
 export class CreateComponent {
   @Input('username') username=""
+
+  constructor(private postService: PostService) {
+  }
+
+  onSubmit(f: NgForm) {
+    const post:Post = {
+      post_date: new Date().toLocaleString(),
+      post_text: f.value.post_text,
+      username: this.username
+    }
+    this.postService.addPost(post)
+  }
 
 }
