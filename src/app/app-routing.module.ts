80d38009a1d8f1ef9d7f251bf53c206e4f743922
Ahmed El herz <69149566+El-Ahmed@users.noreply.github.com>
2023-02-02 18:51:30 +0100

    added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {PostsComponent} from "./posts/posts.component";
+import {CreateComponent} from "./create/create.component";
+import {SignComponent} from "./sign/sign.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:PostsComponent},
+  {path:'posts', redirectTo:""},
+  {path:'posts/:id', component:PostsComponent},
+  {path:'create', component:CreateComponent},
+  {path:'login', component:SignComponent},
+  {path:'**', redirectTo:""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
